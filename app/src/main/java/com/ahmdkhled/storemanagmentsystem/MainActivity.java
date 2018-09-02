0e1740308d4e7877a54753559cff7ecd247b9040
ahmdkhled <ahmdkhled@gmail.com>
2018-09-02 06:28:12 +0200

    add capture activity

--- app/src/main/java/com/ahmdkhled/storemanagmentsystem/MainActivity.java
+++ app/src/main/java/com/ahmdkhled/storemanagmentsystem/MainActivity.java
@@ -1,7 +1,25 @@
 package com.ahmdkhled.storemanagmentsystem;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.TextureView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +27,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
