884593674d2d8d51d9d82966503b78ac04dcd32e
ibrahim karim othman <ibrahimothmanre@gmail.com>
2018-09-15 21:51:14 +0200

    feat:display all categories

---
+++ app/src/main/java/com/ahmdkhled/storemanagmentsystem/adapters/CategoryAdapter.java
@@ -0,0 +1,62 @@
+package com.ahmdkhled.storemanagmentsystem.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.ahmdkhled.storemanagmentsystem.R;
+import com.ahmdkhled.storemanagmentsystem.model.Category;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryHolder> {
+
+    private List<Category> mCategoryList;
+    private Context mContext;
+
+    public CategoryAdapter(List<Category> mCategoryList, Context mContext) {
+        this.mCategoryList = mCategoryList;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public CategoryHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new CategoryHolder(LayoutInflater.from(mContext).
+                inflate(R.layout.categoty_row,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryHolder holder, int position) {
+        Category category = mCategoryList.get(position);
+        holder.mCategoryTxt.setText(category.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mCategoryList != null) return mCategoryList.size();
+        else return 0;
+    }
+
+    public void notifyAdapter(List<Category> mCategoryList){
+        this.mCategoryList = mCategoryList;
+        this.notifyDataSetChanged();
+    }
+
+    class CategoryHolder extends RecyclerView.ViewHolder{
+
+        @BindView(R.id.category_text)
+        TextView mCategoryTxt;
+        public CategoryHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+    }
+}
--- app/src/main/java/com/ahmdkhled/storemanagmentsystem/ui/CategoryActivity.java
+++ app/src/main/java/com/ahmdkhled/storemanagmentsystem/ui/CategoryActivity.java
@@ -1,36 +1,73 @@
 package com.ahmdkhled.storemanagmentsystem.ui;
 
 import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.ahmdkhled.storemanagmentsystem.R;
+import com.ahmdkhled.storemanagmentsystem.adapters.CategoryAdapter;
 import com.ahmdkhled.storemanagmentsystem.data.ProductsContract;
+import com.ahmdkhled.storemanagmentsystem.model.Category;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class CategoryActivity extends AppCompatActivity {
+public class CategoryActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
+    private static final int CATEGORY_LOADER = 13;
+    private static final String TAG = CategoryActivity.class.getSimpleName();
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
     @BindView(R.id.add_category_txt)
     EditText mCategoryTxt;
     @BindView(R.id.add)
     Button mAddCAtegoryBtn;
+    @BindView(R.id.category_recycler_view)
+    RecyclerView mRecyclerView;
+
+    private CategoryAdapter adapter;
+    private List<Category> categoryList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
 
+        // bind views
         ButterKnife.bind(this);
+
+        // setup toolbar
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle(R.string.category_activity_title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // setup recycler view
+        adapter = new CategoryAdapter(categoryList,this);
+        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.VERTICAL));
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setAdapter(adapter);
+
+        getSupportLoaderManager().initLoader(CATEGORY_LOADER,null,this);
+
 
 
         mAddCAtegoryBtn.setOnClickListener(new View.OnClickListener() {
@@ -46,5 +83,40 @@ public class CategoryActivity extends AppCompatActivity {
                     Toast.makeText(CategoryActivity.this, "invalid category", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        Uri uri = (ProductsContract.categoryUri);
+        return new CursorLoader(this, uri,null,null,null,null);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+        if(data != null && data.getCount() > 0){
+            categoryList.clear();
+            data.moveToFirst();
+            do{
+                Category category = new Category();
+                String name = data.getString(data.getColumnIndex(ProductsContract.categoryName));
+                category.setName(name);
+                categoryList.add(category);
+                Log.d(TAG,"cat name "+name);
+            }while (data.moveToNext());
+
+            adapter.notifyAdapter(categoryList);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
